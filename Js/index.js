d3a48f58c94664ac05486a61d1635669ee0f1deb
Darlon <darloncosta050@gmail.com>
2026-03-12 20:25:12 -0300

    att

--- Js/index.js
+++ Js/index.js
@@ -1,24 +1,37 @@
 const taskName = document.getElementById("taskName");
 const btnTask = document.getElementById("btnTask");
 const notInitiated = document.getElementById("notInitiated");
+const started = document.getElementById("started");
+const completed = document.getElementById("completed");
 
 let listTask = [];
+let idCount = 1;
 
 function addNewTask() {
-  if (taskName.value === '') {
+  if (taskName.value === "") {
     alert("task");
     return;
   }
 
   const task = {
-    name: taskName.value
-  }
+    id: idCount,
+    name: taskName.value,
+  };
 
-  listTask.push(task)
+  listTask.push(task);
+  idCount++;
+  renderTask();
 
-  console.log(listTask)
-  taskName.value = ''
+  console.log(listTask);
+  taskName.value = "";
 }
 
+function renderTask() {
+  const render = listTask.map((task) => {
+    return `<div bg-white, p-4, rounded-lg, shadow, mb-2, cursor-move data-id"${task.id}">${task.name}</div>`;
+  });
 
-btnTask.addEventListener('click', addNewTask)
+  notInitiated.innerHTML = render.join("");
+}
+
+btnTask.addEventListener("click", addNewTask);
